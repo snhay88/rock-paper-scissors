0b745f924dbeea3a43b61554cb60ee95cfe36bd3
snhay <64005077+snhay88@users.noreply.github.com>
2025-07-12 12:25:51 +0200

    Implement game-round logic

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice(){
     let num = Math.random();
 
@@ -9,4 +11,58 @@ function getComputerChoice(){
 
 function getHumanChoice() {
     return prompt("choose rock, paper or scissors");
+}
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase()
+    if(humanChoice === "rock" & computerChoice === "paper"){
+        console.log("You lose! Paper beats rock!")
+        computerScore++;
+    }
+    else if((humanChoice === "paper" & computerChoice === "rock")){
+        console.log("You win! Paper beats rock!")
+        humanScore++;
+    } 
+    else if(humanChoice === "scissors" & computerChoice === "rock"){
+        console.log("You lose! Rock beats scissors!")
+        computerScore++;
+    }
+    else if((humanChoice === "rock" & computerChoice === "scissors")){
+        console.log("You win! Rock beats scissors!")
+        humanScore++;
+    }
+    else if(humanChoice === "paper" & computerChoice === "scissors"){
+        console.log("You lose! Scissors beats paper!")
+        computerScore++;
+    }
+    else if((humanChoice === "scissors" & computerChoice === "paper")){
+        console.log("You win! Scissors beats paper!")
+        humanScore++;
+    } else{
+        console.log("It is a draw!")
+    }
+
+}
+
+
+
+function playGame(){
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for(let index = 0; 0 < 5; index++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection)
+
+        console.log(`Human score: ${humanScore}`)
+        console.log(`Computer score: ${computerScore}`)
+
+        
+    }
+
+    const winner = (humanScore > computerScore) ? "Human won!" : "Computer won!"
+
 }

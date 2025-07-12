1359a56db933b51e618e0b7e3f6cd9f1d6efa80c
snhay <64005077+snhay88@users.noreply.github.com>
2025-07-12 12:02:20 +0200

    Implement function that returns human's choice

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function getComputerChoice(){
     let num = Math.random();
 
-    const choice = (num > 2/3) ? "rock" : (num < 2/3 && num > 1/3) ? "paper" : (num < 1/3 ) ? "scissors" : "computer got confused, malfunctioned and didnt pick anything"
-    
-    return choice;
+    return (num > 2/3) ? "rock" :
+    (num < 2/3 && num > 1/3) ? "paper" : 
+    (num < 1/3 ) ? "scissors" : 
+    "computer got confused, malfunctioned and didn't pick anything";
 }
 
+function getHumanChoice() {
+    return prompt("choose rock, paper or scissors");
+}
